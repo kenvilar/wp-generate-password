4d7c8fead1e04c03ab691d81ef465c900015b454
Ken Vilar <kenvilar@gmail.com>
2017-07-12 04:33:41 +0800

    Initial stylesheets and scripts for admin facing

--- admin/class-wpgenerapass-admin.php
+++ admin/class-wpgenerapass-admin.php
@@ -18,4 +18,24 @@ class WPGeneraPass_Admin {
         $this->version = $version;
     }
 
+    public function enqueue_styles() {
+        wp_enqueue_style(
+            $this->plugin_name,
+            plugin_dir_url( __FILE__ ) . 'css/wpgenerapass-admin.css',
+            array(),
+            $this->version,
+            'all'
+        );
+    }
+
+    public function enqueue_scripts() {
+        wp_enqueue_script(
+            $this->plugin_name,
+            plugin_dir_url( __FILE__ ) . 'js/wpgenerapass-admin.js',
+            array( 'jquery' ),
+            $this->version,
+            false
+        );
+    }
+
 }
