1952ed95f93924a5d49b9db2e47f4332beefd4cb
Ken Vilar <kenvilar@gmail.com>
2017-07-11 14:22:33 +0800

    Add plugin deactivator file

---
+++ includes/class-wpgenerapass-deactivator.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Deactivator
+ *
+ * @link       http://kenvilar.com
+ * @since      1.0.0
+ * @package    Wp_Generate_Password
+ * @subpackage Wp_Generate_Password/includes
+ * @author     Ken Vilar <kenvilar@gmail.com>
+ */
+class WPGeneraPass_Deactivator {
+
+    public static function deactivate() {
+
+    }
+
+}
