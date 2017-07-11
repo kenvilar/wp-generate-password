e50dc7742c7fef7de3c691586dcefcac81d5f975
Ken Vilar <kenvilar@gmail.com>
2017-07-12 04:52:03 +0800

    Register initial stylesheets and scripts for public facing

--- public/class-wpgenerapass-public.php
+++ public/class-wpgenerapass-public.php
@@ -18,4 +18,24 @@ class WPGeneraPass_Public {
         $this->version = $version;
     }
 
+    public function enqueue_styles() {
+        wp_enqueue_style(
+            $this->plugin_name,
+            plugin_dir_url( __FILE__ ) . 'css/wpgenerapass-public.css',
+            array(),
+            $this->version,
+            'all'
+        );
+    }
+
+    public function enqueue_scripts() {
+        wp_enqueue_script(
+            $this->plugin_name,
+            plugin_dir_url( __FILE__ ) . 'js/wpgenerapass-public.js',
+            array( 'jquery' ),
+            $this->version,
+            false
+        );
+    }
+
 }
