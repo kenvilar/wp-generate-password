f59f550f59c675adb2d071ee4f7645ddd28a9292
Ken Vilar <kenvilar@gmail.com>
2017-07-11 14:03:47 +0800

    Add plugin file activator

---
+++ includes/class-wpgenerapass-activator.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Activator
+ *
+ * @link       http://kenvilar.com
+ * @since      1.0.0
+ * @package    Wp_Generate_Password
+ * @subpackage Wp_Generate_Password/includes
+ * @author     Ken Vilar <kenvilar@gmail.com>
+ */
+class WPGeneraPass_Activator {
+
+    public static function activate() {
+
+    }
+
+}
