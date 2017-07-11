879292c9e7e1264c61119a3563bb75b99a65b33f
Ken Vilar <kenvilar@gmail.com>
2017-07-12 03:31:08 +0800

    Load the text-domain for translation

--- includes/class-wpgenerapass-i18n.php
+++ includes/class-wpgenerapass-i18n.php
@@ -11,4 +11,12 @@
  */
 class WPGeneraPass_i18n {
 
+    public function load_plugin_textdomain() {
+        load_plugin_textdomain(
+            'wp-generate-password',
+            false,
+            dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+        );
+    }
+
 }
