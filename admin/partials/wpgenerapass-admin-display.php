21644b425d9f4eb6063323ec5e55f6f4e70a463f
Ken Vilar <kenvilar@gmail.com>
2017-07-12 04:36:07 +0800

    Provide an initial admin view

---
+++ admin/partials/wpgenerapass-admin-display.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * @link       http://kenvilar.com
+ * @since      1.0.0
+ * @package    Wp_Generate_Password
+ * @subpackage Wp_Generate_Password/admin/partials
+ */
+?>
