6d80632374090472de8b21934d0a1f35902ec887
Ken Vilar <kenvilar@gmail.com>
2017-07-12 15:17:26 +0800

    Create uninstaller for the plugin

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @link       http://kenvilar.com
+ * @since      1.0.0
+ * @package    Wp_Generate_Password
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
