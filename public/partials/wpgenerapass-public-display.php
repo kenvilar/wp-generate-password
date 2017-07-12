1116a708209130baf0b1931d3ea9b586de4da2fe
Ken Vilar <kenvilar@gmail.com>
2017-07-12 14:19:45 +0800

    Provide public view for plugin

---
+++ public/partials/wpgenerapass-public-display.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * @link       http://kenvilar.com
+ * @since      1.0.0
+ * @package    Wp_Generate_Password
+ * @subpackage Wp_Generate_Password/public/partials
+ */
+?>
